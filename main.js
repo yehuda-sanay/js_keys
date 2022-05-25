ea812da8ec69be6e44933139b20ad0cd7b6e1981
yehuda-sanay <yehudasanay1@gmail.com>
2022-05-25 10:36:05 +0300

    25.05.22

---
+++ main.js
@@ -0,0 +1,38 @@
+// var manger={
+//     firstName: "yehuda",
+//     lestName: "sanay",
+//     age:31,
+//     salary:31000
+// }
+
+// console.log(manger.age);
+
+// var Employee={
+//     firstName:"aviv",
+//     YearofBirth:1991,
+//     email:"aviv1@gmail.com",
+//     adress:{
+//         city:"petah tikva",
+//         street:"hamginim",
+//         number:12
+//     }
+// }
+// var frut={
+//     Name:"apple",
+//     coller:"red",
+//     tape:"princ"
+// }
+
+// console.log(frut.coller);
+// console.log(frut.Name);
+// console.log(frut.tape);
+
+// var cllas={
+//     number:3,
+//     studentQuantity:5,
+//     studentNames:["yehuda","yossi","naty","avi","lior"],
+
+// }
+// for(var i=0;i<cllas.studentNames.length;i++){
+// console.log(cllas.studentNames[i]);
+// }
